a4410aa22e4c10e322645bf7cc7aac7a4eff48bf
Wut <71558901+itswut@users.noreply.github.com>
2024-11-23 00:46:00 +0100

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Tina from "./Tina";
+import TinaQuiz from "./TinaQuiz";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <BrowserRouter basename="/">
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/tina" element={<Tina />} />
+            <Route path="/tina-quiz" element={<TinaQuiz />} />
+            {/* Fallback route for undefined paths */}
+            <Route path="*" element={<div>404 - Page Not Found</div>} />
+        </Routes>
+    </BrowserRouter>
 );
