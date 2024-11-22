e2867890ba82a320f21c562bca7b6a7212be7056
Wut <71558901+itswut@users.noreply.github.com>
2024-11-22 23:00:26 +0100

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,12 +8,12 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/about" element={<div>About</div>} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
