91644a889d08ee738004cfdc3672d38cb3c30c69
Wut <71558901+itswut@users.noreply.github.com>
2024-11-22 23:19:35 +0100

    test

--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './index.css';
+import './App.css';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import secondPage from './secondPage';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter basename={window.location.pathname || ''}>
-      <Routes>
-        <Route path="/portfolio" element={<App />} />
-        <Route path="/about" element={<div>About</div>} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <div className="App">
+     <Router>
+       <Switch>
+         <Route path='/portfolio' exact component={App}/>
+         <Route path='/portfolio/secondpage' exact component={secondPage}/>
+       </Switch>
+     </Router>
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default App;
---
+++ src/secondPage.js
@@ -0,0 +1,9 @@
+function secondPage() {
+  return (
+    <div className="App">
+        <h1>secondPage</h1>
+    </div>
+  );
+}
+
+export default secondPage;
