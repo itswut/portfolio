40faace19317524c933ba1b1c37def5a419a85d9
Wut <71558901+itswut@users.noreply.github.com>
2024-11-22 23:58:14 +0100

    test

--- src/index.js
+++ src/index.js
@@ -3,15 +3,20 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import HomePage from './HomePage';
+import SecondPage from './SecondPage';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Set basename to the repository name (replace "your-repo-name" with your actual repository name)
+const basename = process.env.PUBLIC_URL || '/';
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={window.location.pathname || ''}>
+    <BrowserRouter basename={basename}>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<div>About</div>} />
+        <Route path="/second" element={<SecondPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
