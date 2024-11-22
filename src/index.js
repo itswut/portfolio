82034952a4f44e1a7a0e4ba3c661aa5d9ea533ee
Wut <71558901+itswut@users.noreply.github.com>
2024-11-22 23:09:10 +0100

    test

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={window.location.pathname || ''}>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/about" element={<div>About</div>} />
