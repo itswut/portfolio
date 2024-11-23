5b6446a5648111fe75eef7b44c44d4e521dedeb8
Wut <71558901+itswut@users.noreply.github.com>
2024-11-23 12:17:47 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 
 const App = () => {
     return (
-        <Router basename="/portfolio">
+        <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route 
